3b1f88ce8b2399dc97037b74e0ae4d74410da20b
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:41:46 +0300

    Create 5.2.cpp

---
+++ 5.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    
+    cin >> a >> b;
+    
+    if (a > b)
+    {
+        cout << a / b << endl;
+    }
+    else
+    {
+        cout << "НЕВЕРНОЕ ЗНАЧЕНИЕ" << endl;
+    }
+    
+    return 0;
+}
