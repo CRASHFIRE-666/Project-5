64c6ef62eea645811e05450313d76fae1b119d23
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:40:57 +0300

    Create 5.1.cpp

---
+++ 5.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> 
+#include <cmath>
+using namespace std;
+int main()
+{
+	double b, kb;
+
+	cin >> b;
+
+	cout << b / 1024 << endl;
+
+	system("pause");
+	return 0;
+
+}
